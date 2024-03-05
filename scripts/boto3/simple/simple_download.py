fc087fea8a012f0c91ae2b35fef90705b23e7236
Stefan Jank <stefan@ultihash.io>
2024-03-05 14:17:30 +0100

    Give additional information in usage. Do not create bucket in download.

--- scripts/boto3/simple/simple_download.py
+++ scripts/boto3/simple/simple_download.py
@@ -5,23 +5,24 @@ import sys
 
 if __name__ == "__main__":
     if len(sys.argv) < 2:
-        print("usage: simple_upload.py <endpoint> [file [file ...]]")
+        print("usage: simple_download.py <endpoint> [file [file ...]]")
+        print("\nSimple single-threaded script to outline download from S3 services")
+        print("using boto3 library.")
         sys.exit(1)
 
     # connection to S3 service
     url = sys.argv[1]
-    s3 = boto3.client('s3', endpoint_url=url,
+    source_s3 = boto3.client('s3', endpoint_url=url,
         aws_access_key_id='', aws_secret_access_key='')
 
     # all data will be queried from this bucket
-    bucket_name = "bucket"
-    s3.create_bucket(Bucket=bucket_name)
+    source_bucket_name = "bucket"
 
     # download each file from command line and store it in the current working directory
     for id in range(2, len(sys.argv)):
         file = sys.argv[id]
 
-        response = s3.get_object(Bucket=bucket_name, Key=os.path.basename(file))
+        response = source_s3.get_object(Bucket=source_bucket_name, Key=os.path.basename(file))
         with open(file, 'wb') as f:
             f.write(response['Body'].read())
 
--- scripts/boto3/simple/simple_upload.py
+++ scripts/boto3/simple/simple_upload.py
@@ -6,23 +6,25 @@ import sys
 if __name__ == "__main__":
     if len(sys.argv) < 2:
         print("usage: simple_upload.py <endpoint> [file [file ...]]")
+        print("\nSimple single-threaded script to outline file upload to S3 service")
+        print("using boto3 library.")
         sys.exit(1)
 
     # connection to S3 service
     url = sys.argv[1]
-    s3 = boto3.client('s3', endpoint_url=url,
+    target_s3 = boto3.client('s3', endpoint_url=url,
         aws_access_key_id='', aws_secret_access_key='')
 
     # all data will be stored under
-    bucket_name = "bucket"
-    s3.create_bucket(Bucket=bucket_name)
+    target_bucket_name = "bucket"
+    target_s3.create_bucket(Bucket=target_bucket_name)
 
     # upload each file from command line and output real and effective size
     for id in range(2, len(sys.argv)):
         file = sys.argv[id]
 
         with open(file, 'rb') as f:
-            response = s3.put_object(Bucket=bucket_name, Key=os.path.basename(file), Body=f)
+            response = target_s3.put_object(Bucket=target_bucket_name, Key=os.path.basename(file), Body=f)
 
             headers = response['ResponseMetadata']['HTTPHeaders']
 
