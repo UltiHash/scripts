ff18dc8e3e6844695d97e2f911eac9dde6f75650
Stefan Jank <sj@stiftstrasse.de>
2024-05-22 14:57:50 +0200

    Output space savings on upload. (#10)

--- scripts/boto3/simple/simple_upload.py
+++ scripts/boto3/simple/simple_upload.py
@@ -17,7 +17,13 @@ if __name__ == "__main__":
 
     # all data will be stored under
     target_bucket_name = "bucket"
-    uh_service.create_bucket(Bucket=target_bucket_name)
+    try:
+        uh_service.create_bucket(Bucket=target_bucket_name)
+    except:
+        pass
+
+    total_uploaded = 0
+    total_stored = 0
 
     # upload each file from command line and output real and effective size
     for id in range(2, len(sys.argv)):
@@ -28,7 +34,13 @@ if __name__ == "__main__":
 
             headers = response['ResponseMetadata']['HTTPHeaders']
 
-            uploaded_bytes = float(headers['uh-original-size'])
-            stored_bytes = float(headers['uh-effective-size'])
+            uploaded_bytes = int(headers['uh-original-size'])
+            stored_bytes = int(headers['uh-effective-size'])
+
+            total_uploaded += uploaded_bytes
+            total_stored += stored_bytes
 
             print(f"uploaded {file} with {uploaded_bytes} real size and {stored_bytes} effective size")
+
+    print(f"total uploaded bytes: {total_uploaded}, total stored bytes: {total_stored}")
+    print(f"space savings: {100 * (total_uploaded - total_stored) / total_uploaded:.2f} %")
