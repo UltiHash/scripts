0fb77f4a958b3100f67d2588dd666dc835e42546
MaksimIushchenko <158456131+MaksimIushchenko@users.noreply.github.com>
2024-07-01 09:42:26 +0200

    Add info about PySpark integration (#19)
    * Add info about PySpart integration
    
    * Display Python code better

---
+++ scripts/pyspark/README.md
@@ -0,0 +1,29 @@
+# PySpark Integration
+
+This document provides information regarding the integration between UltiHash and PySpark
+
+## Spark Configuration
+
+Spark relies on [S3A module](https://hadoop.apache.org/docs/stable/hadoop-aws/tools/hadoop-aws/index.html) to provide connectivity with endpoints suppording S3 protocol. Below is an example of the S3A module configuration that connects PySpark with the UltiHash cluster:
+```python
+from pyspark.sql import SparkSession
+
+# Create Spark session
+spark = SparkSession.builder.appName("AppName").getOrCreate()
+sc = spark.sparkContext
+
+# AWS access and secret keys could be any, since authentication is not yet supported by UltiHash
+sc._jsc.hadoopConfiguration().set("fs.s3a.access.key", "mocked")
+sc._jsc.hadoopConfiguration().set("fs.s3a.secret.key", "mocked")
+# The S3 endpoint is a URL pointing to the deployed UltiHash cluster
+# It has to contain the scheme (http or https) and the domain name of the cluster endpoint
+sc._jsc.hadoopConfiguration().set("fs.s3a.endpoint", "https://ultihash.cluster.io")
+# S3 path style access has to be enabled
+sc._jsc.hadoopConfiguration().set("fs.s3a.path.style.access", "true")
+``` 
+In case the UltiHash cluster URL is http-based, need to add the following line into the configuration:
+```
+sc._jsc.hadoopConfiguration().set("fs.s3a.connection.ssl.enabled", "false")
+```
+
+The full example of the PySpark integration could be found [here](./integration.py)
---
+++ scripts/pyspark/integration.py
@@ -0,0 +1,40 @@
+'''
+  This script serves as an example of the integration between PySpark and UltiHash cluster.
+  The script uploads the local CSV file to the UltiHash cluster and then downloads it from there.
+'''
+from pyspark.sql import SparkSession
+from sys import argv
+
+# UltiHash cluster endpoint URL
+s3_endpoint = argv[1]  # For example, https://ultihash.cluster.io
+# Bucket provisioned on the UltiHash cluster in advance
+s3_bucket = argv[2] # For example, "datasets"
+# Path to the local CSV file
+dataset_file_name = argv[3] # For example, "sample_data.csv"
+
+# Create Spark session
+spark = SparkSession.builder.appName("UH Integration Test").getOrCreate()
+sc = spark.sparkContext
+
+# AWS access and secret keys could be any, since authentication is not yet supported by UltiHash
+sc._jsc.hadoopConfiguration().set("fs.s3a.access.key", "mocked")
+sc._jsc.hadoopConfiguration().set("fs.s3a.secret.key", "mocked")
+# The S3 endpoint is a URL pointing to the deployed UltiHash cluster
+sc._jsc.hadoopConfiguration().set("fs.s3a.endpoint", s3_endpoint)
+# S3 path style access has to be enabled
+sc._jsc.hadoopConfiguration().set("fs.s3a.path.style.access", "true")
+
+#In case the UltiHash cluster URL is http-based, need to add the following line into the configuration:
+#sc._jsc.hadoopConfiguration().set("fs.s3a.connection.ssl.enabled", "false")
+
+# Read the local CSV file
+df = spark.read.csv(dataset_file_name,header=True,escape="\"")
+
+# Write the dataset to the bucket
+df.write.format("csv").option("header", "true").mode("overwrite").save("s3a://%s/%s" % (s3_bucket, dataset_file_name))
+
+# Read the dataset from the bucket
+df_new = spark.read.option("header", "true").csv("s3a://%s/%s" % (s3_bucket, dataset_file_name))
+
+# Show the first five lines of the dataset
+df_new.show(5,0)
